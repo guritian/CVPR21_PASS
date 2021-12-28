79fdeaa34fd923da79d34259ca0c326cd34eb931
gh <1179681903@qq.com>
2021-12-28 16:18:20 +0800

    实验代码 还未写完

--- main1.py
+++ main1.py
@@ -18,6 +18,7 @@ from scipy import stats
 from PIL import Image
 
 from PASS import protoAugSSL
+from PASS_for_federated import protoAugSSL_for_federated
 from ResNet import resnet18_cbam
 from myNetwork import network
 from iCIFAR100 import iCIFAR100
@@ -28,7 +29,7 @@ parser.add_argument('--batch_size', default=64, type=int, help='Batch size for t
 parser.add_argument('--print_freq', default=5, type=int, help='print frequency (default: 10)')
 parser.add_argument('--data_name', default='cifar100', type=str, help='Dataset name to use')
 parser.add_argument('--total_nc', default=100, type=int, help='class number for the dataset')
-parser.add_argument('--fg_nc', default=50, type=int, help='the number of classes in first task')
+parser.add_argument('--fg_nc', default=10, type=int, help='the number of classes in first task')
 parser.add_argument('--task_num', default=10, type=int, help='the number of incremental steps')
 parser.add_argument('--learning_rate', default=0.001, type=float, help='initial learning rate')
 parser.add_argument('--protoAug_weight', default=10.0, type=float, help='protoAug loss weight')
@@ -37,6 +38,11 @@ parser.add_argument('--temp', default=0.1, type=float, help='trianing time tempe
 parser.add_argument('--gpu', default='0', type=str, help='GPU id to use')
 parser.add_argument('--save_path', default='model_saved_check/', type=str, help='save files directory')
 
+
+#在联邦学习框架下进行实验的相应设置
+parser.add_argument('--client_num', default=2 , type=int, help='the number of clients')
+parser.add_argument('--rounds', default=10 , type=int, help='联邦学习轮次')
+
 args = parser.parse_args()
 print(args)
 
@@ -44,21 +50,55 @@ print(args)
 def main():
     cuda_index = 'cuda:' + args.gpu
     device = torch.device(cuda_index if torch.cuda.is_available() else "cpu")
-    task_size = int((args.total_nc - args.fg_nc) / args.task_num)  # number of classes in each incremental step
-    file_name = args.data_name + '_' + str(args.fg_nc) + '_' + str(args.task_num) + '*' + str(task_size)
-    feature_extractor = resnet18_cbam()
 
-    model = protoAugSSL(args, file_name, feature_extractor, task_size, device)
-    class_set = list(range(args.total_nc))
 
-    for i in range(args.task_num+1):
-        if i == 0:
-            old_class = 0
-        else:
-            old_class = len(class_set[:args.fg_nc + (i - 1) * task_size])
-        model.beforeTrain(i)
-        model.train(i, old_class=old_class)
-        model.afterTrain()
+    #为client准备本地模型
+    local_model_list = []
+    for i in range(args.client_num):
+        task_size = 10  # number of classes in each incremental step
+        file_name = args.data_name + '_' + str(args.fg_nc) + '_' + "client_index_" +str(i)
+        feature_extractor = resnet18_cbam()
+        local_model_list.append(protoAugSSL_for_federated(args, file_name, feature_extractor, task_size, device))
+
+
+    class_set = list(range(20))
+    task_size = 10
+    #每个client 对自己的模型进行进行预训练
+    #在预训练的过程中  会产生初始的原型  全部发往中心保存
+    for i in range(args.client_num):
+
+        classes = [i*task_size,(i+1)*task_size]
+        local_model_list[i].pre_beforeTrain(classes)
+        local_model_list[i].pretrain(2,classes)
+
+
+    # 中心下发prototype 给每个client
+    # 中心下发 别的client的模型给client i
+    for i in range(args.client_num):
+        local_model_list[i].pre_afterTrain_save()
+    for i in range(args.client_num):
+        local_model_list[i].pre_afterTrain_load()
+
+    #进行联邦学习的轮次
+    for i in range(args.rounds):
+
+        for epoch in range(args.client_num):
+            # 中心下发prototype 给每个client
+            # 中心下发 别的client的模型给client i 进行知识融合 或者这里我们采用的是 增量学习的方式
+            # 拿到上述的数据后 开始进行增量学习
+            local_model_list[i].beforeTrain(classes)
+            local_model_list[i].train(classes)
+
+
+
+
+        #经过上述的 持续学习，原型 得到了新的更新
+        #对从每个client拿到的增量学习后的模型 进行中心聚合,下发 特征提取层，以及得到更新后原型
+
+
+
+
+        return
 
 
     ####### Test ######
